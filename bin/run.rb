c380a952535ffc7816c67c8f8b8104ff30fafb27
RikBala <rik280491@googlemail.com>
2020-01-23 13:40:55 +0000

    update method working

--- bin/run.rb
+++ bin/run.rb
@@ -3,8 +3,9 @@ require_relative '../config/environment'
 cli = CommandLineInterface.new
 cli.greet
 cli.name 
-cli.subscribe 
-cli.delete 
-
+#cli.subscribe
+#cli.total_cost  
+#cli.delete 
+cli.update 
 
 
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -8,8 +8,8 @@ class CommandLineInterface
     
      def name 
         puts "Please enter your name:"
-        @ans_name = gets.chomp
-        @new_customer = Customer.find_or_create_by(name: @ans_name)
+        ans_name = gets.chomp
+        @new_customer = Customer.find_or_create_by(name: ans_name)
      end 
     
     #1, 2, 3, 4 List the four user stories 
@@ -24,7 +24,7 @@ class CommandLineInterface
         ans_sub = Service.find_by(name: ans_subscribe)
         puts "Would you be paying annually? Yes or No"
         ans_annual = gets.chomp   
-        if ans_annual == "Yes"
+        if ans_annual == "Yes" # || "yes"
             @new_subscription = Subscription.find_or_create_by(
                 customer_id: @new_customer.id,
                 service_id: ans_sub.id,
@@ -39,11 +39,61 @@ class CommandLineInterface
         end 
     end 
 
-
+    
     #2 --- As a user, I want to find out the monthly and annual cost for my subscription(s)
+        # def total_cost 
+        #     puts "Would you like to find out your total annual cost for the services you have subscribed to. This service is provided free of charge!"
+        #     ans = gets.chomp 
+        #     if ans == "Yes"
+        #         total_sub_cost = 0 
+        #         cust = Customer.find(@new_customer.id)
+        #         if cust.subscriptions.select{|s| s.annual == true }
+        #             total_sub_cost += cust.services.map{ |s| s.annual_price}.sum 
+        #             if cust.subscriptions.select{|s| s.annual == false}
+        #             total_sub_cost += cust.services.map{ |s| s.monthly_price}.sum 
+        #             end 
+        #         end 
+        #     end  
+        # total_sub_cost
+        # end         
+
+                
+            
 
 
-    #3 --- As a user, I want to update my subscriptions 
+
+
+
+            
+
+
+
+        #     else 
+        #         #move onto next method
+        #     end
+        # end 
+
+    #3 --- As a user, I want to update my subscription from annual to monthly 
+    def update 
+        puts "Would you like to change you subscription payments from monthly to annual, or vice versa?"
+        ans = gets.chomp 
+        up_sub = Subscription.find(@new_customer.id)    
+        if ans == "Yes" #&& annual: false 
+            up_sub.update(annual: true)
+        
+        end        
+    end 
+                
+                
+
+
+
+
+
+
+
+
+
     
 
 
@@ -58,16 +108,22 @@ class CommandLineInterface
 
 
     #4 --- As a user, I want to be able to delete my subscription  
-    def delete 
-        services = Service.all.map {|service| service.name}.uniq
-        puts services 
-        puts "Please enter the name of the service you would like to unsubscribe from:"
-        ans_delete = gets.chomp 
-        ans_delete_service = Service.find_by(name: ans_delete)
-        c_name = Customer.find_by(name: @ans_name)
-        ans_del = Subscription.find_by(customer_id: c_name.id, service_id: ans_delete_service.id)
-        ans_del.destroy
-    end 
+    # def delete 
+    #     services = Service.all.map {|service| service.name}.uniq
+    #     puts services 
+    #     puts "Please enter the name of the service you would like to unsubscribe from:"
+    #     ans_delete = gets.chomp
+    #     #puts "Are you sure?"
+    #     # ans_confirm = gets.chomp
+    #         #if ans_confirm == "Yes" 
+    #     ans_delete_service = Service.find_by(name: ans_delete)
+    #     c_name = Customer.find_by(name: @ans_name)
+    #     ans_del = Subscription.find_by(customer_id: c_name.id, service_id: ans_delete_service.id)
+    #     ans_del.destroy
+    #         #else 
+    #             #puts "You will not regret sticking around"
+    # end 
+
 end 
         
      
